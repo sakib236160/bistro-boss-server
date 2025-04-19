ca54350be1a1cbeca2c5d00ae96b16749b0aacbd
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-19 17:51:21 +0600

    Make user api secure using verify Admin done

--- index.js
+++ index.js
@@ -48,20 +48,50 @@ async function run() {
 
     // midleware varyfay tokan
     const verifyToken = (req,res, next) =>{
-      console.log('inside varify token',req.headers);
+      console.log('inside varify token',req.headers.authorization);
       if(!req.headers.authorization){
-        return res.status(401).send({message: 'forbidden access'});
+        return res.status(401).send({message: 'unauthorized access'});
       }
       const token = req.headers.authorization.split(' ')[1];
-     
-      // next()
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+// user varify admin after varifytoken
+    const varifyAdmin = async(req,res,next)=>{
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next();
     }
 
     // user related api
-    app.get("/users", verifyToken, async (req,res)=>{
+    app.get("/users", verifyToken, varifyAdmin, async (req,res)=>{
       
       const result = await userCollection.find().toArray();
       res.send(result);
+    })
+
+    app.get('/users/admin/:email', verifyToken, async(req,res)=>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin});
     })
     
     app.post("/users", async (req,res)=>{
@@ -75,7 +105,7 @@ async function run() {
       res.send(result);
     })
 
-    app.patch("/users/admin/:id", async(req,res)=>{
+    app.patch("/users/admin/:id", verifyToken, varifyAdmin, async(req,res)=>{
        const id = req.params.id; 
        const filter = {_id: new ObjectId(id)};
        const updatedDoc = {
@@ -87,7 +117,7 @@ async function run() {
        res.send(result);
     })
 
-    app.delete("/users/:id", async(req,res)=>{
+    app.delete("/users/:id",verifyToken, varifyAdmin, async(req,res)=>{
        const id = req.params.id; 
        const query = {_id: new ObjectId(id)}
        const result = await userCollection.deleteOne(query);
