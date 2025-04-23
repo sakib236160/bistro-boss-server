a315e58787e50403630c81da1f6094511fcfe285
Md.Sakib Hossain <hossainsakib965@gmail.com>
2025-04-23 14:15:47 +0600

    Save payment info and delete cart items in the database done

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +36,7 @@ async function run() {
     const userCollection = client.db("bistroDb").collection("users");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payments");
 
 
     // jwt related api
@@ -48,7 +50,7 @@ async function run() {
 
     // midleware varyfay tokan
     const verifyToken = (req,res, next) =>{
-      console.log('inside varify token',req.headers.authorization);
+      // console.log('inside varify token',req.headers.authorization);
       if(!req.headers.authorization){
         return res.status(401).send({message: 'unauthorized access'});
       }
@@ -129,6 +131,45 @@ async function run() {
         const result = await menuCollection.find().toArray();
         res.send(result);
     })
+
+    app.get('/menu/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/menu', verifyToken, varifyAdmin,async(req,res)=>{
+      const item =req.body;
+      const result= await menuCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.patch('/menu/:id', async(req,res)=>{
+      const item = req.body;
+      const id =req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc={
+        $set:{
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+
+      const result = await menuCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/menu/:id', verifyToken, varifyAdmin,async(req,res)=>{
+      const id = req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/reviews', async (req,res)=>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
@@ -153,8 +194,33 @@ async function run() {
       res.send(result);
     })
 
+    // Payment Intern
+    app.post('/create-payment-intent', async(req,res)=>{
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
 
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"], 
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    })
 
+    app.post('/payments',async(req,res)=>{
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      console.log('payment info',payment);
+      const query = {
+        _id:{
+          $in: payment.cartIds.map(id=>new ObjectId(id))
+        }
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({paymentResult,deleteResult});
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
